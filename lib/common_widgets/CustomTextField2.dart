5911891fca9bd06d90a5e875d713502d551bf011
2020bcs0107 <aarthi20BCS107@iiitkottayam.ac.in>
2021-04-07 00:16:09 +0530

    U0-16 organise event form

---
+++ lib/common_widgets/CustomTextField2.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String label;
+  final bool isPassword;
+  final Function(String value) onChanged;
+  final String errorText;
+  final bool isLast;
+  final Function(String value) validator;
+  final int maxlines;
+  final bool hasPrefix;
+  final bool hasNumInput;
+  final bool hasSuffix;
+  final IconData prefixIcon;
+  final String suffixtext;
+
+  CustomTextField({
+    this.suffixtext,
+    this.prefixIcon,
+    this.hasSuffix = false,
+    this.hasNumInput = false,
+    this.hasPrefix = false,
+    this.maxlines = 1,
+    this.label,
+    this.isPassword = false,
+    this.onChanged,
+    this.validator,
+    this.errorText,
+    this.isLast = false,
+  });
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
+    final _focus = FocusScope.of(context);
+    const _simpleBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(12)),
+      borderSide: BorderSide(color: Color(0xFF25242C), width: 2),
+    );
+    final _errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(12)),
+      borderSide: BorderSide(color: _theme.errorColor, width: 2),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: TextFormField(
+        keyboardType:
+            widget.hasNumInput ? TextInputType.number : TextInputType.text,
+        onChanged: widget.onChanged,
+        validator: widget.validator,
+        onEditingComplete: () {
+          if (widget.isLast) {
+            _focus.unfocus();
+          } else {
+            _focus.nextFocus();
+          }
+        },
+        obscureText: widget.isPassword ? !_showPassword : false,
+        textInputAction:
+            widget.isLast ? TextInputAction.done : TextInputAction.next,
+        decoration: InputDecoration(
+          suffixText: widget.hasSuffix ? widget.suffixtext : null,
+          suffixStyle:
+              Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 20),
+          prefixIcon: widget.hasPrefix
+              ? Icon(
+                  widget.prefixIcon,
+                  color: Colors.white,
+                  size: 22,
+                )
+              : null,
+          isDense: true,
+          labelText: widget.label,
+          fillColor: Color(0xFF1E1C24),
+          enabledBorder: _simpleBorder,
+          focusedBorder: _simpleBorder,
+          errorBorder: _errorBorder,
+          focusedErrorBorder: _errorBorder,
+          filled: true,
+          labelStyle: TextStyle(
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w500,
+            color: _theme.accentColor.withOpacity(0.5),
+          ),
+        ),
+        style: TextStyle(
+          color: _theme.accentColor,
+          fontWeight: FontWeight.w500,
+        ),
+        maxLines: widget.maxlines,
+      ),
+    );
+  }
+}
---
+++ lib/event/organise_event.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:ur_buddy_3/common_widgets/CustomTextField2.dart';
+import 'package:ur_buddy_3/common_widgets/custom_flatButton.dart';
+
+
+import 'Organised_row.dart';
+class OrganiseEvent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Organise Event',
+            style: TextStyle(
+              fontSize: 28,
+              fontFamily: 'Montserrat',
+              letterSpacing: 1.5,
+              color: Theme.of(context).accentColor,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_sharp),
+            onPressed: () {},
+          ),
+        ),
+
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/new.jpg'),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 380,
+                child: CustomTextField(
+                  label: 'Event Name',
+                ),
+              ),
+              
+             SizedBox(height: 5.0),
+              OrganisedRow(),
+              SizedBox(height: 5.0),
+              Container(
+                width: 380,
+                child: CustomTextField(
+                  maxlines: 6,
+                  label: 'Description',
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                width: 380,
+                child: CustomTextField(
+                  label: 'Registration Link',
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                width: 320,
+                child: CustomFlatButton(
+                  label: 'ORGANISE',
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+
